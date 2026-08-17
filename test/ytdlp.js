7561dd71611a3a277d9070d4836cf204786fc45a
encos194 <chosthea97@gmail.com>
2026-08-17 23:26:58 +0700

    Add yt-dlp installation instructions
    Added installation instructions for yt-dlp in ytdlp.js.

---
+++ test/ytdlp.js
@@ -0,0 +1,18 @@
+*Cara Install yt-dlp Di Panel*
+
+ Pastikan Panel nya sudah ada ffmpeg ya, Kalau wajib ada Python nya mungkin enggak wajib sih kalian bisa cek dulu ada gak pakai ini 
+
+```python3 --version```
+
+
+Nah di bot langsung pakai command yang $ ( mungkin tiap bot beda )
+
+Langsung aja Pakai command ini
+cd /home/container && curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o yt-dlp && chmod +x yt-dlp
+
+Nah cara pakai nya full path aja yaa contoh 
+
+/home/container/yt-dlp --version
+
+Kalau muncul hasilnya jadi 100% Work 
+Dah sii makasih
